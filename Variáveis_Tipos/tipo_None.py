89bbda6030d1386d99a945460b9afc0ac73460f9
Anne Caroline <carolaragao94@gmail.com>
2022-08-08 08:24:55 -0300

    tipo None

---
+++ Variáveis_Tipos/tipo_None.py
@@ -0,0 +1,14 @@
+"""
+Tipo None
+Representa o tipo sem tipo, ou poderia ser conhecido também como tipo vazio, porém
+falar que é um tipo sem tipo é mais apropriado
+
+--> Sempre especificado com a primeira letra maiúscula
+
+--> Utilizado quando queremos criar uma variável e inicializá-la com um tipo sem tipo, antes
+de recebermos um valor final
+"""
+
+numeros = None
+print(numeros)
+print(type(numeros))
