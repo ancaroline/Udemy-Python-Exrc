b9142c8f54746ed35ca0865cd1fbe49652e9bc5d
Anne Caroline <carolaragao94@gmail.com>
2022-08-08 16:12:49 -0300

    conjuntos(sets)

---
+++ Coleções/conjuntos.py
@@ -0,0 +1,16 @@
+"""
+Conjuntos
+— Estamos fazendo referência à Teoria dos Conjuntos da Matemática
+— Os Conjuntos são nomeados de Sets
+‘Sets’ não possuem valores duplicados;
+Não possuem valores ordenados;
+Elementos não são acessados via índice, ou seja, conjuntos não são indexados;
+Conjuntos são bons para se utilizar quando precisamos armazenar elementos, mas
+não nos importamos com a ordenação deles.
+Quando não precisamos se preocupar com chaves, valores e itens duplicados.
+‘Sets’ são referenciados com chaves {}
+
+Diferença entre Conjuntos e Mapas:
+    — Um dicionário tem chave/valor
+    — Um conjunto tem apenas valor;
+"""
