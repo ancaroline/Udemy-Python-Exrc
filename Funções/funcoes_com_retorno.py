5e6f9ccfd34488654de49c3b8df470e23f5398d8
Anne Caroline <carolaragao94@gmail.com>
2022-08-10 16:45:29 -0300

    funções: sobre return

--- Funções/funcoes_com_retorno.py
+++ Funções/funcoes_com_retorno.py
@@ -35,6 +35,43 @@ def diz_oi():
 alguem = 'Pedro!'
 print(diz_oi() + alguem)
 
+"""
+Return:
+-> Finaliza a função, sai da execução da função
+-> Podemos ter, em uma função, diferentes returns:
+-> Podemos, em uma função, retornar qualquer tipo de dado e até mesmo múltiplos valores
+"""
 
 
+# Exemplo 1
+def diz_oi():
+    return 'Oi!'
+    print('Estou sendo executado após o return')  # Esta linha nunca será executada
 
+
+print(diz_oi())
+
+
+# Exemplo 2
+def nova_funcao():
+    v = False
+    if v:
+        return 4
+    elif v is None:
+        return 3.2
+    return 'b'
+
+
+print(nova_funcao())
+
+
+# Exemplo 3
+def outra_funcao():
+    return 2, 3, 4, 5  # formou uma tupla
+
+
+num1, num2, num3, num4 = outra_funcao()  # desempacotando a tupla
+print(num1, num2, num3, num4)
+
+print(outra_funcao())
+print(type(outra_funcao()))
