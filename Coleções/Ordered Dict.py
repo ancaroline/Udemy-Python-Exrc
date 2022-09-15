09b3d4a22da81538605c2cd914af2cd2d92170d0
Anne Caroline <carolaragao94@gmail.com>
2022-09-15 17:06:22 -0300

    diferença entre dict e ordered dict

--- Coleções/Ordered Dict.py
+++ Coleções/Ordered Dict.py
@@ -17,3 +17,17 @@ dicionario1 = OrderedDict({'a': 1, 'b': 2, 'c': 3})
 
 for chave, valor in dicionario1.items():
     print(f'chave={chave}: valor={valor}')
+
+# Entendendo a diferença entre Dict e Ordered Dict
+
+# Dicionários comuns
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 2, 'a': 1}
+
+print(dict1 == dict2)  # True/False? True, pois a ordem não importa para os dicionários
+
+odict1 = OrderedDict({'a': 1, 'b': 2})
+odict2 = OrderedDict({'a': 2, 'b': 1})
+
+print(odict1 == odict2)  # False, a ordem importa
