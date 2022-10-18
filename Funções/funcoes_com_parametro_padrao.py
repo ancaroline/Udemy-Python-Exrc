e5bd17a4dd4159f75b46d6d7e8e581de5cc6bbfb
Anne Caroline <carolaragao94@gmail.com>
2022-10-18 16:45:13 -0300

    funções com default parameters exemplos

--- Funções/funcoes_com_parametro_padrao.py
+++ Funções/funcoes_com_parametro_padrao.py
@@ -13,6 +13,8 @@ def quadrado(numero):
 
 
 print(quadrado(3))
+
+
 # print(quadrado()) TypeError - precisa ter um parâmetro
 
 
@@ -23,3 +25,49 @@ def exponencial(numero, potencia=2):  # se um parâmetro já recebe um valor, el
 
 print(exponencial(2))
 print(exponencial(3, 5))  # o argumento 5 irá substituir o valor 2
+
+"""
+OBS: Se o usuário passar somente 1 parâmetro, este será atribuído ao parâmetro número
+e será calculado o quadrado deste número;
+Se o usuário passar 2 argumentos, o primeiro será atribuído ao parâmetro número
+e o segundo ao parâmetro potência. Então, será calculada esta potência.
+
+OBS 2: Em funções Pyhton, os parâmetros com valores default (padrão) DEVEM SEMPRE ESTAR ao final da declaração
+"""
+
+
+# Exemplo:
+def obs2(potencia, num=2):
+    return num ** potencia
+
+
+print(obs2(6))  # potencia 6, numero default = 2
+
+
+# Outros exemplos
+
+def soma(num1, num2):
+    return num1 + num2
+
+
+print(soma(4, 3))
+
+
+# print(soma(4)) -> typeError, pois os parâmetros são obrigatórios
+# print(soma()) -> typeError, pois os parâmetros são obrigatórios
+# para não haver esses erros, teria que ter valores padrões
+
+# Exemplo mais complexo
+
+def mostra_informacao(nome='Geek', instrutor=False):
+    if nome == 'Geek' and instrutor:
+        return 'Bem-vindo instrutor Geek!'
+    elif nome == 'Geek':
+        return 'Eu pensei que você era o instrutor'
+    return f'Olá {nome}'
+
+
+print(mostra_informacao())
+print(mostra_informacao(instrutor=True))
+print(mostra_informacao('Panda'))
+print(mostra_informacao(nome='Red Holy Panda'))
