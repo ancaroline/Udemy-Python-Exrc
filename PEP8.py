9dc91bd0bef3d02feb744de749dd8aae78e461c6
Anne Caroline <carolaragao94@gmail.com>
2022-12-14 16:04:06 -0300

    PEP8

---
+++ PEP8.py
@@ -0,0 +1,10 @@
+"""
+PEP8 — propostas de aprimoramento do python, são melhorias para a linguagem Python
+
+Boas práticas
+1 — Utilize CamelCase para nomes de classe
+2 — Utilize nomes em minúsculo, separados por underline para funções ou variáveis
+3 — Utilize 4 espaços para identação. Cuidado com o tab.
+4 - linhas em branco (separar funções - 2 linhas), métodos dentro de uma classe (1 linha em branco)
+5 - Imports
+"""
