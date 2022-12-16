6f22d15f25a94bf641db5cad4154844461a91cf6
Anne Caroline <carolaragao94@gmail.com>
2022-12-16 16:05:44 -0300

    exemplos lambdas

--- Expressoes_Lambdas_Funcoes_Integradas/utilizando_lambdas.py
+++ Expressoes_Lambdas_Funcoes_Integradas/utilizando_lambdas.py
@@ -36,3 +36,31 @@ print(duas(5, 6))
 print(tres(5, 6, 7))
 
 # obs: se passarmos mais argumentos do que parâmetros esperados, teremos TypeError
+
+# Outro exemplo
+autores = ['Rafael Oliveira', 'Anne Rosen', 'Arthur Sanches', 'Ana F. Pimenta', 'Douglas Adams',
+           'Carlos Souza Mart']
+
+print(autores)
+# ordenação por sobrenome
+autores.sort(key=lambda sobrenome: sobrenome.split(' ')[-1].lower())  # parâmetro: sobrenome
+print(autores)
+
+
+# Função Quadrática
+# f(x) = a * x ** 2 + b * x + c
+
+# Definindo a função
+
+def geradora_funcao_quadratica(a, b, c):
+    # Retorna a função f(x) = a * x ** 2 + b * x + c
+    return lambda x: a * x ** 2 + b * x + c
+
+
+teste = geradora_funcao_quadratica(2, 3, -5)
+print(teste(0))  # x valendo 0
+print(teste(1))  # x valendo 1
+print(teste(2))  # x valendo 2
+print(geradora_funcao_quadratica(2, 3, 4)(2))  # x valendo 2
+
+# A gente aplica lambda para fazer filtragem e geração de dados
