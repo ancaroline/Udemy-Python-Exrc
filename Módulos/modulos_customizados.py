b4ec3a28137276d6c8b86a944400cc492c4340f7
Anne Caroline <carolaragao94@gmail.com>
2023-02-02 09:35:14 -0300

    modulos customizados

--- Módulos/modulos_customizados.py
+++ Módulos/modulos_customizados.py
@@ -10,9 +10,13 @@ from funcao_import import soma_impares
 print(soma_impares([1, 2, 3]))
 # Em um módulo não pode ter execução de código, como daquela forma na 'funcao_import'
 
-# Importando todo o módulo
-import funcao_import
+# Importando todo o módulo (Temos acesso a TODOS os elementos do módulo)
+import funcao_import as fcp
 
+# acessando e imprimindo uma variável contida no módulo
+print(fcp.lista)
+
+print(fcp.soma_impares(fcp.lista))
 
 
 
